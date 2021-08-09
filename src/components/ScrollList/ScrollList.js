c3e1af220c4c8de2277229fe65c276259d24713d
krzysztof ziecina <kziecina@airnauts.com>
2021-08-09 15:11:39 +0200

    feat: base

--- src/components/ScrollList/ScrollList.js
+++ src/components/ScrollList/ScrollList.js
@@ -1,41 +1,85 @@
-import React, { useEffect, useRef, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import {createStyles, makeStyles} from "@material-ui/core";
 
 export const ScrollList = (props) => {
   const classes = useStyles();
-  const ref = useRef(null);
+  const ref = useRef([]);
+  const screen = useRef(null);
   const [items, setItems] = useState([0, 1, 2, 3, 4, 5, 6]);
 
-  let wasRight = false;
-  const handleObserverRight = (entries) => {
-    entries.forEach(entry => {
-      if (entry.boundingClientRect && entry.rootBounds) {
-        const isRight = entry.boundingClientRect.x > entry.rootBounds.x;
+  let previousY = 0
+  let previousRatio = 0
 
-        if (entry.isIntersecting) {
-          if (wasRight) {
+  //lewo
+  let wasRight = false;
+  const handleObserverRight = useCallback((entries) => {
+    entries.forEach(entry => {
+     console.log(window.pageXOffset);
+
+      const currentY = entry.boundingClientRect.x
+      const currentRatio = entry.intersectionRatio
+      const isIntersecting = entry.isIntersecting
+      // Scrolling down/up
+      if (currentY < previousY) {
+        if (currentRatio > previousRatio && isIntersecting) {
           const tmp = items[0] + 7;
           setItems((prev) => [...prev.slice(1), tmp]);
+          // console.log('Scrolling right enter')
+        } else {
+          // console.log('Scrolling right leave')
+        }
+      } else if (currentY > previousY && isIntersecting) {
+        if (currentRatio < previousRatio) {
+          // console.log('Scrolling left leave')
+        } else {
+          // console.log('Scrolling left enter')
         }
       }
-      wasRight = isRight;
-    }
-  })}
+      previousY = currentY
+      previousRatio = currentRatio
+    })
+  })
 
+
+
+
+
+
+
+
+
+  //     console.log(entry.boundingClientRect);
+  //     if(!entry.isIntersecting){
+  //       return;
+  //     }
+  //     if (entry.boundingClientRect && entry.rootBounds) {
+  //     // console.log('lewo', entry.boundingClientRect.x);
+  //     // console.log('lewo', entry.rootBounds.x);
+  //       const isRight = entry.boundingClientRect.x > entry.rootBounds.x;
+  //         if (wasRight) {
+  //         const tmp = items[0] + 7;
+  //         setItems((prev) => [...prev.slice(1), tmp]);
+  //           console.log(entry.target)
+  //         }
+  //     wasRight = isRight;
+  //   }
+  // })}
+
+  // prawo
   let wasLeft = false;
   const handleObserverLeft = (entries) => {
     entries.forEach(entry => {
+        if (entry.isIntersecting) {
       if (entry.boundingClientRect && entry.rootBounds) {
         const isLeft = entry.boundingClientRect.x < entry.rootBounds.x;
 
-        if (entry.isIntersecting) {
           if (wasLeft) {
               const tmp = items[items.length-1] - 7;
               setItems((prev) => [tmp, ...prev.slice(0,-1)]);
-          }
         }
         wasLeft = isLeft;
       }
+          }
     })}
 
   useEffect(()=>{
@@ -44,34 +88,38 @@ export const ScrollList = (props) => {
 
   useEffect(() => {
     let optionsRight = {
-      root: document.querySelector("#scrollArea"),
       rootMargin: "0px",
-      threshold: [1],
+      threshold: 0.5
     };
     let optionsLeft = {
-      root: document.querySelector("#scrollArea"),
       rootMargin: "0px",
-      threshold: [1],
+      threshold: 0.5,
     };
     const observerLeft = new IntersectionObserver(handleObserverLeft, optionsLeft);
     const observerRight = new IntersectionObserver(handleObserverRight, optionsRight);
-    if (ref.current) observerLeft.observe(ref.current.children[0]);
-    if (ref.current) observerRight.observe(ref.current.children[ref.current.children.length-1]);
+    // if (ref.current) observerLeft.observe(ref.current.children[0]);
+    // if (ref.current) ref.current.forEach((card)=>{
+    //   observerRight.observe(card);
+    // })
+    if (ref.current) observerRight.observe(ref.current[6]);
+    if (ref.current) observerLeft.observe(ref.current[0]);
     return () => {
-      if (ref.current) observerLeft.unobserve(ref.current);
-      if (ref.current) observerRight.unobserve(ref.current);
+      // if (ref.current) observerLeft.unobserve(ref.current);
+      if (ref.current) ref.current.forEach((card)=>{
+        observerRight.unobserve(card);
+      })
     };
-  }, [handleObserverLeft, handleObserverRight]);
+  }, [ handleObserverRight]);
 
   return (
-    <div
-      ref={ref} style={{ width: `${300000+((Math.floor(items[items.length - 1] / items.length)) * 300*items.length)}px`}}
+    <div style={{ width: `${300000+((Math.floor(items[items.length - 1] / items.length)) * 300*items.length)}px`}}
       className={classes.wrapper}
     >
       {items.map((item, i) => (
         <div
           style={{position: "absolute", left: `${150000 + (item * 300)}px`, top: "0"}}
           key={item}
+          ref={el => (ref.current[i] = el)}
         >
           <p>{item}</p>
           <div className={classes.mockup}/>
@@ -83,6 +131,10 @@ export const ScrollList = (props) => {
 
 const useStyles = makeStyles((theme) =>
   createStyles({
+    screen:{
+      width:'100vw',
+      height:'100vh',
+    },
     mockup: {
       boxSizing: 'border-box',
       width: '300px',
