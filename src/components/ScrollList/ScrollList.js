0a238b14910c10768d89aa0ca93cba5c14497a08
krzysztof ziecina <kziecina@airnauts.com>
2021-08-12 10:00:28 +0200

    feat: working solution with scrollListener and throttling

--- src/components/ScrollList/ScrollList.js
+++ src/components/ScrollList/ScrollList.js
@@ -1,77 +1,26 @@
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {createStyles, makeStyles} from "@material-ui/core";
+import { throttle } from 'throttle-debounce';
 
 export const ScrollList = (props) => {
   const classes = useStyles();
   const ref = useRef([]);
-  const screen = useRef(null);
   const [items, setItems] = useState([0, 1, 2, 3, 4, 5, 6]);
 
-  let prevScrollPosition = window.pageXOffset;
-
-  //lewo
-  let wasRight = false;
-  const handleObserverRight = useCallback((entries) => {
-    entries.forEach(entry => {
-      let newScrollPosition = window.pageXOffset;
-      //scroll Right;
-      if (newScrollPosition > prevScrollPosition) {
-        if (entry.isIntersecting) {
-          const tmp = items[0] + 7;
-          setItems((prev) => [...prev.slice(1), tmp]);
-          console.log('pravo')
-        }
-      }
-      prevScrollPosition = newScrollPosition;
-    })
-  })
-
-  // lewo
-
-  const handleObserverLeft = (entries) => {
-    entries.forEach(entry => {
-      let newScrollPosition = window.pageXOffset;
-      //scroll Right;
-      if (newScrollPosition < prevScrollPosition) {
-        if (entry.isIntersecting) {
-          const tmp = items[items.length-1] - 7;
-          setItems((prev) => [tmp, ...prev.slice(0,-1)]);
-          console.log('levo')
-        }
-      }
-      prevScrollPosition = newScrollPosition;
-    })}
-
-  // useEffect(()=>{
-  //   window.scrollTo(150000, 0);
-  // },[])
-
-  let stop = false;
+  // let prevScrollPosition = window.pageXOffset;
+  const moveFunc = throttle(100, (num) => {
+    let newScrollPosition = window.pageXOffset + window.innerWidth;
+      console.log('items', items[6]*300)
+      const max = Math.floor(newScrollPosition / 300)
+      setItems((prev) => [max-6, max-5, max-4, max-3, max-2, max-1, max]);
+  });
 
   useEffect(() => {
-    window.addEventListener('scroll', function(e) {
-
-      if(!stop) {
-        let newScrollPosition = window.pageXOffset + window.innerWidth;
-        //scroll Right;
-        console.log('scroll pos',newScrollPosition)
-        if (items[6]*300 < newScrollPosition) {
-          console.log('items', items[6]*300)
-          //do sth thats connected with scrollX
-          const max = Math.floor(newScrollPosition / 300)
-          const tmp = items[0] + 7;
-          setItems((prev) => [max-6, max-5, max-4, max-3, max-2, max-1, max]);
-        }
-        prevScrollPosition = newScrollPosition;
-        stop=true;
-      setTimeout(() => {
-        stop = false;
-      },100)
-      }
-    });
-    return () => {
-    };
-  }, [ handleObserverRight]);
+    window.addEventListener('scroll', moveFunc)
+   return () => {
+     window.removeEventListener('scroll', moveFunc)
+   }
+  }, [ moveFunc]);
 
   return (
     <div style={{ width: `${((Math.floor(items[items.length - 1] / items.length)) * 300*items.length)}px`}}
