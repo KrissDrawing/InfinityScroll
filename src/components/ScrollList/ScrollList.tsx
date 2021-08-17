9a393799f050a27e1a7af41eb51300808ef51a00
krzysztof ziecina <kziecina@airnauts.com>
2021-08-17 16:55:41 +0200

    feat: add scroll to today, and display different dates

--- src/components/ScrollList/ScrollList.tsx
+++ src/components/ScrollList/ScrollList.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { FixedSizeList as List } from "react-window";
 import { DateTime } from "luxon";
 import { Box, Button, createStyles, makeStyles, Typography } from "@material-ui/core";
@@ -11,6 +11,12 @@ export const ScrollList = () => {
 
   const [listWidth, setListWidth] = useState(0);
   const [week, setWeek] = useState(false);
+
+  const listRef = useRef<List>(null)
+
+  const scrollToToday = () => {
+    if (listRef && listRef.current) listRef.current.scrollToItem(initialOffset)
+  }
 
   useLayoutEffect(() => {
     const resizeListener = () => {
@@ -25,17 +31,22 @@ export const ScrollList = () => {
   }, []);
 
   const Column = ({index, style}: { index: number, style: any }) => (
-    <div style={style} className={classes.mockup}>
+    <div style={style} className={classes.listItem}>
       {week
         ? (
           <Box>
-            <Typography>{DateTime.now().plus({week: (4 * index) - initialOffset}).startOf("week").monthLong}</Typography>
-          <WeeksColumn monday={DateTime.now().plus({week: (4 * index) - initialOffset}).startOf("week")}/>
+            <Box className={classes.dateWrapper}>
+              <Typography>{DateTime.now().plus({week: (4 * index) - 4 * initialOffset}).startOf("week").monthLong}</Typography>
+              <Typography>{DateTime.now().plus({week: (4 * index) - 4 * initialOffset}).startOf("week").year}</Typography>
+            </Box>
+            <WeeksColumn monday={DateTime.now().plus({week: (4 * index) - 4 * initialOffset}).startOf("week")}/>
           </Box>
         ) : (
           <Box>
-            <Typography>{DateTime.now().plus({week: index - initialOffset}).weekNumber}</Typography>
-            <Typography>{DateTime.now().plus({week: index - initialOffset}).startOf("week").toLocaleString()}</Typography>
+            <Box className={classes.dateWrapper}>
+              <Typography>{DateTime.now().plus({week: index - initialOffset}).weekNumber}</Typography>
+              <Typography>{DateTime.now().plus({week: index - initialOffset}).year}</Typography>
+            </Box>
             <DayColumn monday={DateTime.now().plus({week: index - initialOffset}).startOf("week")}/>
           </Box>
         )}
@@ -44,30 +55,55 @@ export const ScrollList = () => {
 
   return (
     <>
-      <Button onClick={() => setWeek((prevState) => !prevState)}>
-        {week ? "days" : "weeks"}
-      </Button>
       <List
-        height={150}
+        height={200}
         itemCount={1000}
         itemSize={300}
         layout="horizontal"
         width={listWidth}
         initialScrollOffset={initialOffset * 300}
+        ref={listRef}
       >
         {Column}
       </List>
+      <Box className={classes.buttonWrapper}>
+        <Button
+          color="secondary"
+          variant="outlined"
+          onClick={scrollToToday}
+        >
+          Today
+        </Button>
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => setWeek((prevState) => !prevState)}
+        >
+          {week ? "days" : "weeks"}
+        </Button>
+      </Box>
     </>
-  )
+  );
 };
 
 const useStyles = makeStyles(() =>
   createStyles({
-    mockup: {
+    listItem: {
       boxSizing: "border-box",
       background: "lightblue",
       border: "1px solid teal",
     },
+    buttonWrapper: {
+      display: "flex",
+      justifyContent: "flex-end",
+      margin: "20px",
+      gap: "20px",
+    },
+    dateWrapper: {
+      display: "flex",
+      gap: "20px",
+      justifyContent: "center",
+    }
   }),
 );
 
